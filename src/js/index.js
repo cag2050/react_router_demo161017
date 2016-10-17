fef11a77786f0f59a953c8e68a562652ed63e509
chenanguo <chenanguo@le.com>
2016-10-17 16:01:07 +0800

    m

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Left from './component/Left';
+import Right from './component/Right';
+import Bottom from './component/Bottom';
 
-ReactDOM.render(<Left/>, document.getElementById("page"));
-console.log("console.log输出的内容");
-document.write("document.write输出的内容");
+ReactDOM.render(<div className="container">
+    <div className="header">header</div>
+    <div className="middle">
+        <div className="con_left"><Left /></div>
+        <div className="content">content</div>
+        <div className="con_right"><Right /></div>
+    </div>
+    <div className="bottom"><Bottom /></div>
+</div>, document.getElementById("page"));
+// console.log("console.log输出的内容");
+// document.write("document.write输出的内容");
